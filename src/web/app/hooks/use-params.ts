aea24f8d0d8755c4337624c0d9c451e05b5db500
dev <dev@matterv.com>
2024-12-03 23:07:35 -0800

    refactor

--- src/web/app/hooks/use-params.ts
+++ src/web/app/hooks/use-params.ts
@@ -5,51 +5,36 @@
  *
  * Copyright Oxide Computer Company
  */
-import { hashKey } from '@tanstack/react-query'
-import { useMemo } from 'react'
-import { useParams, type Params } from 'react-router-dom'
+import { hashKey } from "@tanstack/react-query";
+import { useMemo } from "react";
+import { useParams, type Params } from "react-router-dom";
 
-import { invariant } from '~/util/invariant'
+import { invariant } from "~/util/invariant";
 
 const err = (param: string) =>
-  `Param '${param}' not found in route. You might be rendering a component under the wrong route.`
+  `Param '${param}' not found in route. You might be rendering a component under the wrong route.`;
 
-type AllParams = Readonly<Params<string>>
+type AllParams = Readonly<Params<string>>;
 
 // default of never is required to prevent the highly undesirable property that if
 // you don't pass any arguments, the result object thinks every property is defined
 export const requireParams =
   <K extends string = never>(...requiredKeys: K[]) =>
   (params: AllParams) => {
-    const requiredParams: { [k in K]?: string } = {}
+    const requiredParams: { [k in K]?: string } = {};
     for (const k of requiredKeys) {
-      const value = params[k]
-      if (process.env.NODE_ENV !== 'production') {
-        invariant(k in params && value, err(k))
+      const value = params[k];
+      if (process.env.NODE_ENV !== "production") {
+        invariant(k in params && value, err(k));
       }
-      requiredParams[k] = value
+      requiredParams[k] = value;
     }
-    return requiredParams as { readonly [k in K]: string }
-  }
+    return requiredParams as { readonly [k in K]: string };
+  };
 
-export const getProjectSelector = requireParams('project')
-export const getFloatingIpSelector = requireParams('project', 'floatingIp')
-export const getInstanceSelector = requireParams('vmId')
-export const getVpcSelector = requireParams('project', 'vpc')
-export const getFirewallRuleSelector = requireParams('project', 'vpc', 'rule')
-export const getVpcRouterSelector = requireParams('project', 'vpc', 'router')
-export const getVpcRouterRouteSelector = requireParams('project', 'vpc', 'router', 'route')
-export const getVpcSubnetSelector = requireParams('project', 'vpc', 'subnet')
-export const getSiloSelector = requireParams('silo')
-export const getSiloImageSelector = requireParams('image')
-export const getIdpSelector = requireParams('silo', 'provider')
-export const getProjectImageSelector = requireParams('project', 'image')
-export const getProjectSnapshotSelector = requireParams('project', 'snapshot')
-export const requireSledParams = requireParams('sledId')
-export const requireUpdateParams = requireParams('version')
-export const getIpPoolSelector = requireParams('pool')
-export const requiredHostParams = requireParams('hostId')
-export const requiredStorageParams = requireParams('storageId')
+export const getInstanceSelector = requireParams("vmId");
+export const requiredHostParams = requireParams("hostId");
+export const requiredStorageParams = requireParams("storageId");
 
 /**
  * Turn `getThingSelector`, a pure function on a params object, into a hook
@@ -67,31 +52,15 @@ export const requiredStorageParams = requireParams('storageId')
  * if you use the object as a whole.
  */
 function useSelectedParams<T>(getSelector: (params: AllParams) => T) {
-  const sel = getSelector(useParams())
+  const sel = getSelector(useParams());
   // eslint-disable-next-line react-hooks/exhaustive-deps
-  return useMemo(() => sel, [hashKey([sel])])
+  return useMemo(() => sel, [hashKey([sel])]);
 }
 
 // Wrappers for RR's `useParams` that guarantee (in dev) that the specified
 // params are present. Only the specified keys end up in the result object, but
 // we do not error if there are other params present in the query string.
 
-export const useFloatingIpSelector = () => useSelectedParams(getFloatingIpSelector)
-export const useProjectSelector = () => useSelectedParams(getProjectSelector)
-export const useProjectImageSelector = () => useSelectedParams(getProjectImageSelector)
-export const useProjectSnapshotSelector = () =>
-  useSelectedParams(getProjectSnapshotSelector)
-export const useInstanceSelector = () => useSelectedParams(getInstanceSelector)
-export const useVpcSelector = () => useSelectedParams(getVpcSelector)
-export const useVpcRouterSelector = () => useSelectedParams(getVpcRouterSelector)
-export const useVpcRouterRouteSelector = () => useSelectedParams(getVpcRouterRouteSelector)
-export const useVpcSubnetSelector = () => useSelectedParams(getVpcSubnetSelector)
-export const useFirewallRuleSelector = () => useSelectedParams(getFirewallRuleSelector)
-export const useSiloSelector = () => useSelectedParams(getSiloSelector)
-export const useSiloImageSelector = () => useSelectedParams(getSiloImageSelector)
-export const useIdpSelector = () => useSelectedParams(getIdpSelector)
-export const useSledParams = () => useSelectedParams(requireSledParams)
-export const useUpdateParams = () => useSelectedParams(requireUpdateParams)
-export const useIpPoolSelector = () => useSelectedParams(getIpPoolSelector)
-export const useHostParams = () => useSelectedParams(requiredHostParams)
-export const useStorageParams = () => useSelectedParams(requiredStorageParams)
+export const useInstanceSelector = () => useSelectedParams(getInstanceSelector);
+export const useHostParams = () => useSelectedParams(requiredHostParams);
+export const useStorageParams = () => useSelectedParams(requiredStorageParams);
